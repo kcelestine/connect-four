2a0c9728ba9637e6fd708fae4f6f42f924993d00
Khadijah <khadijah.celestine@gmail.com>
2023-04-01 15:46:30 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,4 +6,18 @@ terraform {
       name = "first-one"
     }
   }
+  
+  resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.b.id
+  acl    = "private"
+}
 }
